25b6eca7c0e34347262aefe74c1531e26065beda
somadsul <somnathadsul@gmail.com>
2020-10-02 00:27:45 +0530

    Update propritary files as we don't have official vendor we need to go old way

--- rootdir/bin/device_check.sh
+++
@@ -1,9 +0,0 @@
-#!/sbin/sh
-
-if ! grep "FN" /vendor/build.prop
-then
-rm -rf /system/system/app/NfcNci
-rm -f /system/system/etc/libnfc-nci.conf
-rm -f /system/system/lib/libnfc-nci*
-rm -rf /system/system/priv-app/Tag
-fi
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,50 +1,38 @@
 #!/bin/bash
-#
-# Copyright (C) 2018-2019 The LineageOS Project
-#
+# Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
-#
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2018
+# Required!
+VENDOR=samsung
+DEVICE_COMMON=universal7885-common
+DEVICE="jackpot2lte jackpotlte"
 
-# Load extract_utils and do some sanity checks
+# Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-source "${HELPER}"
+. "$HELPER"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
+# Initialize the helper
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "jackpot2lte"
+write_headers "$DEVICE"
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt true
 
-# Finish
+# Offline charging
+write_makefiles "$MY_DIR"/proprietary-files-lpm.txt true
+
+# We are done!
 write_footers
-
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
